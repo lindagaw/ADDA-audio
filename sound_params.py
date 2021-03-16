bbcb7306d278b99cf3debc2f820ee7a229a93031
Ashley Gao <gaoye0718@gmail.com>
2021-03-16 13:20:00 -0400

    after 100 epches

--- sound_params.py
+++ sound_params.py
@@ -28,12 +28,12 @@ d_output_dims = 5
 d_model_restore = "snapshots/ADDA-critic-final.pt"
 
 # params for training network
-num_gpu = 2
-num_epochs_pre = 500
+num_gpu = 4
+num_epochs_pre = 100
 log_step_pre = 20
 eval_step_pre = 20
 save_step_pre = 100
-num_epochs = 500
+num_epochs = 100
 log_step = 100
 save_step = 100
 manual_seed = None
