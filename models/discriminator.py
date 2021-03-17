bf9d576aee00910a9127005ac364bb41f758056f
Ashley Gao <gaoye0718@gmail.com>
2021-03-17 14:14:19 -0400

    WIP

--- models/discriminator.py
+++ models/discriminator.py
@@ -13,40 +13,6 @@ class Discriminator(nn.Module):
         self.restored = False
 
         self.encoder = nn.Sequential(
-            # 1st conv layer
-            # input [48 x 272]
-            # output [46 x 3072]
-            nn.Conv1d(in_channels=1, out_channels=3072, kernel_size=3),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2, padding=0),
-            nn.Dropout(),
-
-            # 2nd conv layer
-            # input [23, 3072]
-            # output [21, 6144]
-            nn.Conv1d(in_channels=3072, out_channels=6144, kernel_size=3, stride=2),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2, padding=0),
-            nn.Dropout(),
-
-            # 3rd conv layer
-            # input [10, 6144]
-            # output [8, 6144]
-            nn.Conv1d(in_channels=6144, out_channels=6144, kernel_size=3, stride=2),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2, padding=0),
-            nn.Dropout(),
-
-            # 4th conv layer
-            # input [4, 6144]
-            # output [2, 3072]
-            nn.Conv1d(in_channels=6144, out_channels=3072, kernel_size=1, stride=1),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=1, stride=1, padding=0),
-            nn.Dropout(),
-        )
-
-        self.fc1 = nn.Sequential(
             nn.Flatten(),
             nn.Linear(3072, 4096),
             nn.Dropout(),
@@ -56,12 +22,13 @@ class Discriminator(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.Dropout(),
-            nn.Linear(4096, 5)
+            nn.Linear(4096, 5),
+            nn.LogSoftmax()
         )
 
     def forward(self, input):
         """Forward the discriminator."""
         """Forward the LeNet."""
-        conv_out = self.encoder(input)
-        out = self.fc1(conv_out.view(-1, 3072 * 1))
+        out = self.encoder(input)
+        #out = self.fc1(conv_out.view(-1, 3072 * 1))
         return out
