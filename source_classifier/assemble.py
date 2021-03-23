2f95063da2279d7c0841b2d9a54489596c925efa
Ashley Gao <gaoye0718@gmail.com>
2021-03-23 19:57:55 -0400

    WIP

--- source_classifier/assemble.py
+++ source_classifier/assemble.py
@@ -2,11 +2,22 @@
 
 import numpy as np
 import torc
-
+import params
 import load_chopped_source_model, load_second_half_chopped_source_model
 
-xs_test = np.load('data//Conflict//' + 'conflict_testing_xs.npy')
-ys_test = np.load('data//Conflict//' + 'conflict_testing_ys.npy')
+#xs_test = np.load('data//Conflict//' + 'conflict_testing_xs.npy')
+#ys_test = np.load('data//Conflict//' + 'conflict_testing_ys.npy')
 
-# load conv 1:
-conv_1 = load_chopped_source_model(1)
+'''
+for a given input x, if probe_1(x) == 1 ==> exit at enforcer_1(x)
+elif: probe_2(x) == 1 ==> exit at enforcer_2(x)
+elif: probe_3(x) == 1 ==> exit at enforcer_3(x)
+elif: probe_4(x) == 1 ==> exit at enforcer_4(x)
+else: exit at rest_of_src_classifier(x)
+'''
+feats_after_enforcers = params.feats_after_enforcers
+preds_after_enforcers = params.preds_after_enforcers
+preds_after_probes = params.preds_after_probes
+
+s = load_second_half_chopped_source_model(conv=4)
+print(s.shape)
--- source_classifier/params.py
+++ source_classifier/params.py
@@ -1,6 +1,27 @@
 first_half = '..//..//chopped_models//second_half//'
 second_half = '..//..//chopped_models//second_half//'
 
+all_feats_by_conv_1_after_enforcer = 'result//all_feats_by_conv_1_after_enforcer.pt'
+all_feats_by_conv_2_after_enforcer = 'result//all_feats_by_conv_2_after_enforcer.pt'
+all_feats_by_conv_3_after_enforcer = 'result//all_feats_by_conv_3_after_enforcer.pt'
+all_feats_by_conv_4_after_enforcer = 'result//all_feats_by_conv_4_after_enforcer.pt'
+feats_after_enforcers = [all_feats_by_conv_1_after_enforcer, all_feats_by_conv_2_after_enforcer, \
+                        all_feats_by_conv_3_after_enforcer, all_feats_by_conv_4_after_enforcer]
+
+all_preds_by_conv_1_after_enforcer = 'result//all_preds_by_conv_1_after_enforcer.pt'
+all_preds_by_conv_2_after_enforcer = 'result//all_preds_by_conv_2_after_enforcer.pt'
+all_preds_by_conv_3_after_enforcer = 'result//all_preds_by_conv_3_after_enforcer.pt'
+all_preds_by_conv_4_after_enforcer = 'result//all_preds_by_conv_4_after_enforcer.pt'
+preds_after_enforcers = [all_preds_by_conv_1_after_enforcer, all_preds_by_conv_2_after_enforcer, \
+                        all_preds_by_conv_3_after_enforcer, all_preds_by_conv_4_after_enforcer]
+
+all_preds_by_conv_1_after_probe = 'result//all_preds_by_conv_1_after_probe.pt'
+all_preds_by_conv_2_after_probe = 'result//all_preds_by_conv_2_after_probe.pt'
+all_preds_by_conv_3_after_probe = 'result//all_preds_by_conv_3_after_probe.pt'
+all_preds_by_conv_4_after_probe = 'result//all_preds_by_conv_4_after_probe.pt'
+preds_after_probes = [all_preds_by_conv_1_after_probe, all_preds_by_conv_2_after_probe, \
+                        all_preds_by_conv_3_after_probe, all_preds_by_conv_3_after_probe]
+
 data_root = "data"
 dataset_mean_value = 0.5
 dataset_std_value = 0.5
