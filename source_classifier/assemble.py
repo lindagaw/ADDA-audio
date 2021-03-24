ff243bec0fd2967e116c7aa9978dfbbab7e029fa
Ashley Gao <gaoye0718@gmail.com>
2021-03-23 21:46:25 -0400

    WIP

--- source_classifier/assemble.py
+++ source_classifier/assemble.py
@@ -9,6 +9,14 @@ from utils import init_random_seed
 xs_test = np.load('data//Conflict//' + 'conflict_testing_xs.npy')
 ys_test = np.load('data//Conflict//' + 'conflict_testing_ys.npy')
 
+import sys
+import os
+from tensorflow import keras
+from keras.models import load_model
+from keras.models import Sequential
+
+model = load_model('..//model.hdf5')
+
 '''
 for a given input x, if probe_1(x) == 1 ==> exit at enforcer_1(x)
 elif: probe_2(x) == 1 ==> exit at enforcer_2(x)
@@ -27,16 +35,21 @@ ys_pred = []
 
 for index in range(0, len(xs_test)):
     x = xs_test[index]
-
+    print('processing ' + str(index) + ' th element ...')
     for conv in range(0, 4):
         probe = preds_after_probes[conv][index]
-        print(probe)
-        break
-    break
+        if probe == 1:
+            y_pred = preds_after_enforcers[conv][index]
+            ys_pred.append(y_pred)
+            break
 
+    y_pred = np.squeeze(model.predict(np.expand_dims(x, axis=0)))
+    ys_pred.append(y_pred)
 
-print(xs_test.shape)
-print(ys_test.shape)
-print(feats_after_enforcers[0].shape)
-print(preds_after_enforcers[0].shape)
-print(preds_after_probes[0].shape)
+ys_pred = np.asarray(ys_pred)
+print(ys_pred.shape)
+#print(xs_test.shape)
+#print(ys_test.shape)
+#print(feats_after_enforcers[0].shape)
+#print(preds_after_enforcers[0].shape)
+#print(preds_after_probes[0].shape)
