1fe099d48103760a95f8e23a3902889c42fc417c
Ashley Gao <gaoye0718@gmail.com>
2021-03-23 21:03:59 -0400

    WIP

--- source_classifier/assemble.py
+++ source_classifier/assemble.py
@@ -6,8 +6,8 @@ import params
 from load_1st_half_chopped_source_model import load_chopped_source_model
 from load_2nd_half_chopped_source_model import load_second_half_chopped_source_model
 from utils import init_random_seed
-#xs_test = np.load('data//Conflict//' + 'conflict_testing_xs.npy')
-#ys_test = np.load('data//Conflict//' + 'conflict_testing_ys.npy')
+xs_test = np.load('data//Conflict//' + 'conflict_testing_xs.npy')
+ys_test = np.load('data//Conflict//' + 'conflict_testing_ys.npy')
 
 '''
 for a given input x, if probe_1(x) == 1 ==> exit at enforcer_1(x)
@@ -18,9 +18,12 @@ else: exit at rest_of_src_classifier(x)
 '''
 init_random_seed(params.manual_seed)
 
-feats_after_enforcers = params.feats_after_enforcers
-preds_after_enforcers = params.preds_after_enforcers
-preds_after_probes = params.preds_after_probes
+# index 0 --> conv 1
+feats_after_enforcers = [torch.load(path) for path in params.feats_after_enforcers]
+preds_after_enforcers = [torch.load(path) for path in params.preds_after_enforcers]
+preds_after_probes = [torch.load(path) for path in params.preds_after_probes]
 
-s = load_second_half_chopped_source_model(conv=4)
-print(s.shape)
+print(xs.shape)
+print(feats_after_enforcers[0].shape)
+print(preds_after_enforcers[0].shape)
+print(preds_after_probes[0].shape)
