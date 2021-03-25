badb0f87a765ded65212c2ca0fcbd16aec7d2eb5
Ashley Gao <gaoye0718@gmail.com>
2021-03-25 17:31:22 -0400

    WIP

--- models/lenet.py
+++ models/lenet.py
@@ -35,7 +35,7 @@ class LeNetEncoder(nn.Module):
             # output [8, 6144]
             #nn.Conv1d(in_channels=6144, out_channels=6144, kernel_size=3, stride=2),
             #nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2, padding=0),
+            #nn.MaxPool1d(kernel_size=2, stride=2, padding=0),
             #nn.Dropout(),
 
             # 4th conv layer
@@ -43,7 +43,7 @@ class LeNetEncoder(nn.Module):
             # output [2, 3072]
             nn.Conv1d(in_channels=6144, out_channels=3072, kernel_size=1, stride=1),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=1, stride=1, padding=0),
+            nn.MaxPool1d(kernel_size=4, stride=1, padding=0),
             nn.Dropout(),
         )
 
