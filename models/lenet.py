aa7af0d4db201305e1e54226e3b7e51f77c98ba2
Ashley Gao <gaoye0718@gmail.com>
2021-03-25 15:47:31 -0400

    WIP

--- models/lenet.py
+++ models/lenet.py
@@ -1,4 +1,4 @@
-"""LeNet model for ADDA."""
+"""LeNet, the source cnn, for ADDA."""
 
 import torch.nn.functional as F
 from torch import nn
@@ -15,41 +15,42 @@ class LeNetEncoder(nn.Module):
 
         self.encoder = nn.Sequential(
             # 1st conv layer
-            # input [1 x 28 x 28]
-            # output [20 x 12 x 12]
-            nn.Conv1d(in_channels=272, out_channels=20, kernel_size=5),
+            # input [48 x 272]
+            # output [46 x 3072]
+            nn.Conv1d(in_channels=272, out_channels=3072, kernel_size=5),
             nn.MaxPool1d(kernel_size=2),
             nn.ReLU(),
-            # 2nd conv layer
-            # input [20 x 12 x 12]
-            # output [50 x 4 x 4]
-            nn.Conv1d(20, 3072, kernel_size=5),
-            nn.Dropout(),
+            #nn.Dropout(),
+
+            # 4th conv layer
+            # input [4, 6144]
+            # output [2, 3072]
+            nn.Conv1d(in_channels=3072, out_channels=3072, kernel_siz=5),
+            nn.Dropout()
             nn.MaxPool1d(kernel_size=2),
-            nn.ReLU()
+            nn.ReLU(),
         )
-        #self.fc1 = nn.Linear(50 * 4 * 4, 500)
 
         self.fc1 = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(3072, 4096)
+            nn.Linear(3072, 4096),
             #nn.Linear(4096, 5)
         )
 
     def forward(self, input):
         """Forward the LeNet."""
         conv_out = self.encoder(input)
-        feat = self.fc1(conv_out.view(-1, 3072))
+        feat = self.fc1(conv_out.view(-1, 3072 * 1))
         return feat
 
 
 class LeNetClassifier(nn.Module):
     """LeNet classifier model for ADDA."""
-
     def __init__(self):
         """Init LeNet encoder."""
         super(LeNetClassifier, self).__init__()
-        self.fc2 = nn.Linear(500, 2)
+        self.fc2 = nn.Linear(4096, 2)
+        #self.fc2 = nn.Linear(4096, 5)
 
     def forward(self, feat):
         """Forward the LeNet classifier."""
