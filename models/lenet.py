35f99a3bd0a10e5c871ac6e1e4fe7b6f63eca816
Ashley Gao <gaoye0718@gmail.com>
2021-03-25 17:44:26 -0400

    WIP

--- models/lenet.py
+++ models/lenet.py
@@ -17,53 +17,34 @@ class LeNetEncoder(nn.Module):
             # 1st conv layer
             # input [48 x 272]
             # output [46 x 3072]
-            nn.Conv1d(in_channels=272, out_channels=3072, kernel_size=3),
+            nn.Conv1d(in_channels=272, out_channels=3072, kernel_size=5),
+            nn.MaxPool1d(kernel_size=2),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2, padding=0),
-            nn.Dropout(),
-
-            # 2nd conv layer
-            # input [23, 3072]
-            # output [21, 6144]
-            nn.Conv1d(in_channels=3072, out_channels=6144, kernel_size=3, stride=2),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2, padding=0),
-            nn.Dropout(),
-
-            # 3rd conv layer
-            # input [10, 6144]
-            # output [8, 6144]
-            #nn.Conv1d(in_channels=6144, out_channels=6144, kernel_size=3, stride=2),
-            #nn.ReLU(),
-            #nn.MaxPool1d(kernel_size=2, stride=2, padding=0),
             #nn.Dropout(),
 
             # 4th conv layer
             # input [4, 6144]
             # output [2, 3072]
-            nn.Conv1d(in_channels=6144, out_channels=3072, kernel_size=1, stride=1),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=1, stride=1, padding=0),
+            nn.Conv1d(in_channels=3072, out_channels=3072, kernel_siz=5),
             nn.Dropout(),
+            nn.MaxPool1d(kernel_size=2),
+            nn.ReLU(),
         )
 
         self.fc1 = nn.Sequential(
             nn.Flatten(),
             nn.Linear(3072, 4096),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.Dropout()
             #nn.Linear(4096, 5)
         )
 
     def forward(self, input):
         """Forward the LeNet."""
         conv_out = self.encoder(input)
-        feat = self.fc1(conv_out.view(-1, 3072 * 2))
+        print('--------------')
+        print(conv_out.shape)
+        feat = self.fc1(conv_out.view(-1, 3072 * 1))
+        print(feat.shape)
+        print('--------------')
         return feat
 
 
