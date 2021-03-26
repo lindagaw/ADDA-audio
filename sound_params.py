63dbea17d33b398533d586641778fdf307c0b024
Ashley Gao <gaoye0718@gmail.com>
2021-03-25 22:11:08 -0400

    LETNET worked

--- sound_params.py
+++ sound_params.py
@@ -31,17 +31,17 @@ d_model_restore = "snapshots/" + src_dataset + "-to-" + tgt_dataset + "ADDA-crit
 
 # params for training network
 num_gpu = 4
-num_epochs_pre = 10
+num_epochs_pre = 50
 log_step_pre = 20
 eval_step_pre = 20
 save_step_pre = 100
-num_epochs = 10
+num_epochs = 50
 log_step = 100
 save_step = 100
 manual_seed = None
 
 # params for optimizing models
-d_learning_rate = 1e-5
-c_learning_rate = 1e-5
+d_learning_rate = 1e-4
+c_learning_rate = 1e-4
 beta1 = 0.5
 beta2 = 0.9
