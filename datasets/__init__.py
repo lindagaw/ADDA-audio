70b6cb0093caa1d535736970a3251477a11b052d
Ashley Gao <gaoye0718@gmail.com>
2021-03-28 14:44:27 -0400

    WIP

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,5 +1,3 @@
-from .mnist import get_mnist
-from .usps import get_usps
 from .emotion import get_emotion
 from .conflict import get_conflict
 
@@ -9,7 +7,7 @@ from .conv_2_activations import get_conv_2_activations
 from .conv_3_activations import get_conv_3_activations
 from .conv_4_activations import get_conv_4_activations
 
-__all__ = (get_usps, get_mnist, get_emotion, get_conflict, \
+__all__ = (get_emotion, get_conflict, \
             get_conv_0_activations, get_conv_1_activations, \
             get_conv_2_activations, get_conv_3_activations, \
             get_conv_4_activations)
