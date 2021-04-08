b87772d3dd8951de108bb6200894086937fe9062
Ashley Gao <gaoye0718@gmail.com>
2021-04-08 12:04:03 -0400

    WIP

--- core/test.py
+++ core/test.py
@@ -46,7 +46,7 @@ def eval_tgt(encoder, classifier, data_loader):
     loss /= len(data_loader)
     acc /= len(data_loader.dataset)
     #f1 /= len(data_loader.dataset)
-    f1 = get_f1(ys_pred, ys_true, 'binary')
+    f1 = get_f1(ys_pred, ys_true, 'macro')
     f1_weighted = get_f1(ys_pred, ys_true, 'weighted')
 
     print("Avg Loss = {}, F1 = {:2%}, Weighted F1 = {:2%}".format(loss, f1, f1_weighted))
