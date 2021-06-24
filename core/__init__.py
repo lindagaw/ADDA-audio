4eb027a133f750631384170e052e28f089b5f8d8
Ashley Gao <gaoye0718@gmail.com>
2021-06-24 17:52:48 -0400

    WIP

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,5 @@
 from .adapt import train_tgt, train_tgt_classifier, train_tgt_encoder
 from .pretrain import eval_src, train_src
-from .test import eval_tgt
-from .adda_funcs import eval_src_encoder, eval_tgt_encoder, train_src_encoder, train_tgt_encoder, eval_ADDA, train_tgt_classifier, get_distribution
+from .test import eval_tgt, get_distribution, eval_ADDA
 
-__all__ = (eval_src, train_src, train_tgt, eval_tgt, eval_src_encoder, eval_tgt_encoder, train_src_encoder, train_tgt_encoder, eval_ADDA, train_tgt_classifier, get_distribution)
+__all__ = (eval_src, train_src, train_tgt, eval_tgt)
