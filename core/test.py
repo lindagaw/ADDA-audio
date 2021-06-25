2bde1915cd8f904ab63c77364b84c602bacd5e93
Ashley Gao <gaoye0718@gmail.com>
2021-06-25 13:31:12 -0400

    WIP

--- core/test.py
+++ core/test.py
@@ -102,13 +102,13 @@ def is_in_distribution(sample, inv, mean, mahalanobis_mean, mahalanobis_std):
         return False
 
 
-def eval_tgt_ood(src_encoder, tgt_encoder, classifier, src_data_loader, dataloader):
+def eval_tgt_ood(src_encoder, tgt_encoder, classifier, src_data_loader, tgt_dataloader):
 
     src_inv, src_mean, src_mahalanobis_mean, src_mahalanobis_std = \
-                get_distribution(src_encoder, target_encoder, src_invdata_loader)
+                get_distribution(src_encoder, target_encoder, src_inv, src_data_loader)
 
     tgt_inv, tgt_mean, tgt_mahalanobis_mean, tgt_mahalanobis_std = \
-                get_distribution(src_encoder, target_encoder, tgt_invdata_loader)
+                get_distribution(src_encoder, target_encoder, tgt_inv, tgt_data_loader)
 
     """Evaluation for target encoder by source classifier on target dataset."""
     # set eval state for Dropout and BN layers
