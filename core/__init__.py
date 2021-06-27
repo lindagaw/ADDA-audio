9948badc683dc7a2659bac966eb6b893bed3577e
Ashley Gao <gaoye0718@gmail.com>
2021-06-27 11:31:22 -0400

    WIP

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,4 @@
-from .adapt import train_tgt, train_tgt_classifier, train_tgt_encoder
+from .adapt import train_tgt, train_tgt_encoder
 from .pretrain import eval_src, train_src
 from .test import eval_tgt, eval_tgt_ood
 
