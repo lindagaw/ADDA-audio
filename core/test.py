baf82492d5a773c7a54b95ffdf29f8fe592c6a92
Ashley Gao <gaoye0718@gmail.com>
2021-06-26 21:34:53 -0400

    WIP

--- core/test.py
+++ core/test.py
@@ -91,8 +91,8 @@ def get_distribution(src_encoder, tgt_encoder, data_loader):
     return inv, mean, mahalanobis_mean, mahalanobis_std
 
 def is_in_distribution(sample, inv, mean, mahalanobis_mean, mahalanobis_std):
-    upper_coeff = 5000
-    lower_coeff = 5000
+    upper_coeff = 15000
+    lower_coeff = 15000
 
     sample = sample.detach().cpu().numpy()
 
