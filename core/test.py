ce1a66c2a70ef39119e6e8ec424e795084c88f1e
Ashley Gao <gaoye0718@gmail.com>
2021-06-26 20:19:16 -0400

    squeeze the vector

--- core/test.py
+++ core/test.py
@@ -140,9 +140,9 @@ def eval_tgt_ood(src_encoder, tgt_encoder, classifier, src_data_loader, tgt_data
                 not is_in_distribution(image, src_inv, src_mean, src_mahalanobis_mean, src_mahalanobis_std):
                 continue
             elif is_in_distribution(image, tgt_inv, tgt_mean, tgt_mahalanobis_mean, tgt_mahalanobis_std):
-                y_pred = np.argmax(classifier(tgt_encoder(torch.unsqueeze(image, dim=0))).detach().cpu().numpy())
+                y_pred = np.argmax(np.squeeze(classifier(tgt_encoder(torch.unsqueeze(image, dim=0))).detach().cpu().numpy()))
             else:
-                y_pred = np.argmax(classifier(src_encoder(torch.unsqueeze(image, dim=0))).detach().cpu().numpy())
+                y_pred = np.argmax(np.squeeze(classifier(src_encoder(torch.unsqueeze(image, dim=0))).detach().cpu().numpy()))
 
             ys_pred.append(y_pred)
             ys_true.append(label)
