561636859b54d58f8a936eab0652a8b3c934925f
Ashley Gao <gaoye0718@gmail.com>
2021-06-29 18:52:39 -0400

    wip

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 from .discriminator import Discriminator
 from .lenet import LeNetClassifier, LeNetEncoder
 from .lenet2 import LeNet2Classifier, LeNet2Encoder
-#from .cnn_galatea import GalateaEncoder, GalateaClassifier
+from .cnn_galatea import GalateaEncoder, GalateaClassifier
 from .cnn_andromeda import AndromedaEncoder, AndromedaClassifier
 
 from .auriel import AurielEncoder, AurielClassifier
@@ -11,7 +11,7 @@ from .dione import DioneEncoder, DioneClassifier
 from .essentia import EssentiaEncoder, EssentiaClassifier
 
 
-__all__ = (#GalateaClassifier, GalateaEncoder, AndromedaEncoder, AndromedaClassifier, \
+__all__ = (GalateaClassifier, GalateaEncoder, AndromedaEncoder, AndromedaClassifier, \
             Discriminator, \
             AurielEncoder, AurielClassifier, BeatriceEncoder, BeatriceClassifier, \
             CielEncoder, CielClassifier, DioneEncoder, DioneClassifier, \
