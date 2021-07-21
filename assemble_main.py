cdfd9a32e6b0011e54cd4654d753d9edccb2521a
Ashley Gao <gaoye0718@gmail.com>
2021-07-21 19:26:06 -0400

    WIP

--- assemble_main.py
+++ assemble_main.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     for x in xs_testing:
         x = np.expand_dims(x, axis=0)
 
-        for i in range(0, 3):
+        for i in range(0, 2):
 
             activation = torch.from_numpy(convs[i].predict(x))
             activation = activation.reshape((activation.shape[0], activation.shape[2], activation.shape[1]))
@@ -87,7 +87,7 @@ if __name__ == '__main__':
                 y_preds.append(int(y_pred))
                 break
             
-            if i == 2:
+            if i == 1:
                 y_pred = np.argmax(original_convs.predict(x))
                 y_preds.append(int(y_pred))
 
