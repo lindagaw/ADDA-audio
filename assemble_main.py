6c2a8265344d8b4a1f1bb1fb8b5d0088b63d6c92
Ashley Gao <gaoye0718@gmail.com>
2021-07-21 17:01:16 -0400

    WIP

--- assemble_main.py
+++ assemble_main.py
@@ -1,9 +1,12 @@
-"""Main script for ADDA."""
-
 from six.moves import urllib
 opener = urllib.request.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 urllib.request.install_opener(opener)
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
 
 import sound_params as params
 from core import eval_src, eval_tgt, train_src, train_tgt, eval_tgt_ood, train_tgt_classifier
@@ -13,9 +16,13 @@ from models import AurielEncoder, BeatriceEncoder, CielEncoder, DioneEncoder
 from models import AurielClassifier, BeatriceClassifier, CielClassifier, DioneClassifier
 from utils import get_data_loader, init_model, init_random_seed
 
+from source_classifier import load_chopped_source_model, load_second_half_chopped_source_model
+
 import sys
 
 def trio(tgt_classifier_net, tgt_encoder_net, src_dataset, tgt_dataset, conv):
+    print('loading pretrained trio after conv ' + str(conv) + '...')
+
     tgt_classifier = init_model(net=tgt_classifier_net,
                                 restore= str(conv) + "_snapshots/" + \
                                 src_dataset + "-ADDA-target-classifier-final.pt")
@@ -34,11 +41,41 @@ if __name__ == '__main__':
 
     tgt_classifier_1, tgt_encoder_1, critic_1 = \
         trio(AurielClassifier(), AurielEncoder(), 'CONV_1_ACTIVATIONS', 'CONV_1_ACTIVATIONS', 1)
-    '''
     tgt_classifier_2, tgt_encoder_2, critic_2 = \
         trio(BeatriceClassifier(), BeatriceEncoder(), 'CONV_2_ACTIVATIONS', 'CONV_2_ACTIVATIONS', 2)
     tgt_classifier_3, tgt_encoder_3, critic_3 = \
         trio(CielClassifier(), CielEncoder(), 'CONV_3_ACTIVATIONS', 'CONV_3_ACTIVATIONS', 3)
     tgt_classifier_4, tgt_encoder_4, critic_4 = \
         trio(DioneClassifier(), DioneEncoder(), 'CONV_4_ACTIVATIONS', 'CONV_4_ACTIVATIONS', 4)
-    '''
+
+    tgt_classifiers = [tgt_classifier_1, tgt_classifier_2, tgt_classifier_3, tgt_classifier_4]
+    tgt_encoders = [tgt_encoder_1, tgt_encoder_2, tgt_encoder_3, tgt_encoder_4]
+    critics = [critic_1, critic_2], critic_3, critic_4
+
+    xs_testing = np.load('..//..//Datasets//CONFLICT//conflict_testing_xs.npy')
+    ys_testing = np.load('..//..//Datasets//CONFLICT//conflict_testing_ys.npy')
+
+    print('{} of testing xs shape; {} of testing ys shape'.format(xs_testing.shape, ys_testing.shape))
+
+    after_conv1 = load_chopped_source_model(conv=1)
+    after_conv2 = load_chopped_source_model(conv=2)
+    after_conv3 = load_chopped_source_model(conv=3)
+    after_conv4 = load_chopped_source_model(conv=4)
+
+    convs = [after_conv1, after_conv2, after_conv3, after_conv4]
+    original_convs = tf.keras.models.load_model('..//model.hdf5')
+
+    for x in xs_testing:
+        x = np.expand_dims(x, axis=0)
+
+        for i in range(0, 1):
+
+            activation = convs[i].predict(x)
+            encoded = tgt_encoders[i](activation)
+            criticized = critics[i](activation)
+
+            print(criticized)
+
+
+
+
