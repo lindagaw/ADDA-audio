55296f63f6f4a360ca5f70b812c613a36d67e0d4
Ashley Gao <gaoye0718@gmail.com>
2021-07-21 11:04:28 -0400

    WIP

--- testing.py
+++ testing.py
@@ -48,35 +48,45 @@ testing_ys = np.load(os.path.join('..', '..', 'Datasets', 'CONFLICT', 'conflict_
 
 tgt_classifier_conv0 = GalateaClassifier()
 tgt_encoder_conv0 = GalateaEncoder()
-critic_conv0 = Discriminator()
+critic_conv0 = Discriminator(input_dims=params.d_input_dims,
+                                      hidden_dims=params.d_hidden_dims,
+                                      output_dims=params.d_output_dims)
 tgt_classifier_conv0.load_state_dict(tgt_classifiers[0])
 tgt_encoder_conv0.load_state_dict(tgt_encoder[0])
 critic_conv0.load_state_dict(critics[0])
 
 tgt_classifier_conv1 = AurielClassifier()
 tgt_encoder_conv1 = AurielEncoder()
-critic_conv1 = Discriminator()
+critic_conv1 = Discriminator(input_dims=params.d_input_dims,
+                                      hidden_dims=params.d_hidden_dims,
+                                      output_dims=params.d_output_dims)
 tgt_classifier_conv1.load_state_dict(tgt_classifiers[1])
 tgt_encoder_conv1.load_state_dict(tgt_encoder[1])
 critic_conv1.load_state_dict(critics[1])
 
 tgt_classifier_conv2 = BeatriceClassifier()
 tgt_encoder_conv2 = BeatriceEncoder()
-critic_conv2 = Discriminator()
+critic_conv2 = Discriminator(input_dims=params.d_input_dims,
+                                      hidden_dims=params.d_hidden_dims,
+                                      output_dims=params.d_output_dims)
 tgt_classifier_conv2.load_state_dict(tgt_classifiers[2])
 tgt_encoder_conv2.load_state_dict(tgt_encoder[2])
 critic_conv2.load_state_dict(critics[2])
 
 tgt_classifier_conv3 = CielClassifier()
 tgt_encoder_conv3 = CielEncoder()
-critic_conv3 = Discriminator()
+critic_conv3 = Discriminator(input_dims=params.d_input_dims,
+                                      hidden_dims=params.d_hidden_dims,
+                                      output_dims=params.d_output_dims)
 tgt_classifier_conv3.load_state_dict(tgt_classifiers[3])
 tgt_encoder_conv3.load_state_dict(tgt_encoder[3])
 critic_conv3.load_state_dict(critics[3])
 
 tgt_classifier_conv4 = DioneClassifier()
 tgt_encoder_conv4 = DioneEncoder()
-critic_conv4 = Discriminator()
+critic_conv4 = Discriminator(input_dims=params.d_input_dims,
+                                      hidden_dims=params.d_hidden_dims,
+                                      output_dims=params.d_output_dims)
 tgt_classifier_conv4.load_state_dict(tgt_classifiers[4])
 tgt_encoder_conv4.load_state_dict(tgt_encoder[4])
 critic_conv4.load_state_dict(critics[4])
@@ -91,4 +101,5 @@ for x in testing_xs:
     for i in range(0, 4):
         encoded = tgt_encoder_convs[i](x)
         criticized = critic_convs[i](x)
+        print(criticized)
         classified = tgt_classifier_convs[i](x)
