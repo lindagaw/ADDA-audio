5813b87e893c433e071bc991b1b894728628ec65
Ashley Gao <gaoye0718@gmail.com>
2021-08-09 23:20:04 -0400

    WIP

--- ood_baseline/ood_with_original_src_model.py
+++ ood_baseline/ood_with_original_src_model.py
@@ -8,13 +8,10 @@ from sklearn.metrics import f1_score
 model = tf.keras.models.load_model('..//..//model.hdf5')
 
 # convert shape (,2) to (,5)
-def conflict_to_anger(ys):
+def from_one_hot(ys):
     returned = []
     for y in ys:
-        if np.argmax(y) == 0:
-            returned.append(1)
-        else:
-            returned.append(0)
+        returned.append(np.argmax(y))
 
     return np.asarray(returned)
 
@@ -29,10 +26,10 @@ ys_test_src = np.load(emotion_path+'emotion_testing_ys.npy')
 
 xs_train_tgt = np.load(conflict_path+'conflict_training_xs.npy')
 xs_test_tgt = np.load(conflict_path+'conflict_testing_xs.npy')
-ys_train_tgt = conflict_to_anger(np.load(conflict_path+'conflict_training_ys.npy'))
-ys_test_tgt = conflict_to_anger(np.load(conflict_path+'conflict_testing_ys.npy'))
+ys_train_tgt = from_one_hot(np.load(conflict_path+'conflict_training_ys.npy'))
+ys_test_tgt = from_one_hot(np.load(conflict_path+'conflict_testing_ys.npy'))
 
 # calculate the empirical mean of xs_train
-ys_predicted = model.predict(xs_test_tgt)
+ys_predicted = from_one_hot(model.predict(xs_test_tgt))
 print(ys_predicted)
 print(f1_score(ys_test_tgt, ys_predicted, average='weighted'))
